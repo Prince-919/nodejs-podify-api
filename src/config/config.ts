e4e5af0ffeb2fdd7c35db6781ee0d238dd28554b
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2024-12-22 18:26:04 +0530

    send email to verify user

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,8 @@ dotenv.config();
 const _config = {
   port: process.env.PORT,
   databaseUrl: process.env.MONGODB_CONNECTION_STRING,
+  mailtrapUser: process.env.MAILTRAP_USER,
+  mailtrapPass: process.env.MAILTRAP_PASS,
 };
 
 const config = Object.freeze(_config);
--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,11 +1,26 @@
+import { RequestHandler } from "express";
+import nodemailer from "nodemailer";
 import { User } from "@/models";
 import { CreateUser } from "@/types";
-import { RequestHandler } from "express";
+import { config } from "@/config";
 
 class UserController {
   create: RequestHandler = async (req: CreateUser, res) => {
     const { name, email, password } = req.body;
     const user = await User.create({ name, email, password });
+    var transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: config.mailtrapUser,
+        pass: config.mailtrapPass,
+      },
+    });
+    transport.sendMail({
+      to: user.email,
+      from: "auth@myapp.com",
+      html: "<h1>123426</h1>",
+    });
     res.status(201).json({ user });
   };
 }
