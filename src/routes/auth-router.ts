e9138f98ee29b9e06d72f34e88efc9447eaa9b90
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-01-14 23:17:15 +0530

    upload image to local dir

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import { userController } from "@/controllers";
 import { isValidPassResetToken, mustAuth, validate } from "@/middlewares";
+import formidable from "formidable";
+import path from "path";
+import fs from "fs";
 import {
   CreateUserSchema,
   SignInValidationSchema,
@@ -37,6 +40,29 @@ router.post(
 );
 router.get("/is-auth", mustAuth, (req, res) => {
   res.json({ profile: req.user });
+});
+router.post("/update-profile", async (req, res) => {
+  if (!req.headers["content-type"]?.startsWith("multipart/form-data;")) {
+    res.status(422).json({ error: "Only accepts form-data!" });
+    return;
+  }
+  const dir = path.join(__dirname, "../public/profiles");
+
+  try {
+    await fs.readdirSync(dir);
+  } catch (error) {
+    await fs.mkdirSync(dir);
+  }
+
+  const form = formidable({
+    uploadDir: dir,
+    filename(name, ext, part, form) {
+      return Date.now() + "_" + part.originalFilename;
+    },
+  });
+  form.parse(req, (err, fields, files) => {
+    res.json({ uploaded: true });
+  });
 });
 
 export default router;
