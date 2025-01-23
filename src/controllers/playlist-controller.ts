286c7ab969586168fa3c71f276872792d485f242
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-01-23 08:51:18 +0530

    add updated playlist api

--- src/controllers/playlist-controller.ts
+++ src/controllers/playlist-controller.ts
@@ -1,5 +1,5 @@
 import { Audio, Playlist } from "@/models";
-import { CreatePlaylistRequest } from "@/types";
+import { CreatePlaylistRequest, UpdatePlaylistRequest } from "@/types";
 import { RequestHandler } from "express";
 
 class PlaylistController {
@@ -28,10 +28,42 @@ class PlaylistController {
     });
   };
 
-  updatePlaylist: RequestHandler = async (
-    req: CreatePlaylistRequest,
-    res
-  ) => {};
+  updatePlaylist: RequestHandler = async (req: UpdatePlaylistRequest, res) => {
+    const { id, title, item, visibility } = req.body;
+
+    const playlist = await Playlist.findOneAndUpdate(
+      {
+        _id: id,
+        owner: req.user?.id,
+      },
+      { title, visibility },
+      { new: true }
+    );
+
+    if (!playlist) {
+      res.status(404).json({ error: "Playlist not found!" });
+      return;
+    }
+
+    if (item) {
+      const audio = await Audio.findById(item);
+      if (!audio) {
+        res.status(404).json({ error: "Audio not found!" });
+        return;
+      }
+      await Playlist.findByIdAndUpdate(playlist._id, {
+        $addToSet: { items: item },
+      });
+    }
+
+    res.status(200).json({
+      playlist: {
+        id: playlist._id,
+        title: playlist.title,
+        visibility: playlist.visibility,
+      },
+    });
+  };
 }
 
 const playlistController = new PlaylistController();
