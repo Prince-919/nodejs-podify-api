d15840b2b14bf2c7978c15936421733dc2a60c2d
Prince Sharma <86801714+Prince-919@users.noreply.github.com>
2025-01-30 23:32:09 +0530

    update password reset token name

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
 export { default as User } from "./user-model";
 export { default as EmailVerificationToken } from "./email-verification-token-model";
-export { default as PasswordResetToken } from "./password-reset-token";
+export { default as PasswordResetToken } from "./password-reset-token-model";
 export { default as Audio } from "./audio-model";
 export { default as Favorite } from "./favorite-model";
 export { default as Playlist } from "./playlist-model";
